63b99e5c94f84e26b8b6da26da8adc1088a75029
The-Pain-01 <rubangomoses000@gmail.com>
2026-02-12 20:07:59 +0200

    Update typing.js

--- commands/typing.js
+++ commands/typing.js
@@ -1,8 +1,9 @@
 export default {
   name: 'typing',
-  ownerOnly: true,
-  async execute(sock, m, args) {
-    global.botModes.typing = args[0] === 'on';
-    await sock.sendMessage(m.chat, { text: '⌨️ Typing modifié.' }, { quoted: m });
+  async execute(sock, m) {
+    await sock.sendPresenceUpdate('composing', m.chat);
+    setTimeout(() => {
+      sock.sendPresenceUpdate('paused', m.chat);
+    }, 5000);
   }
 };
