00378df68ab0c04eb6912d6f0fddb5b412af9815
The-Pain-01 <rubangomoses000@gmail.com>
2026-03-17 02:31:16 +0200

    Delete curse.js

--- commands/curse.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'curse',
-  async execute(sock, m) {
-    await sock.sendMessage(m.chat, {
-      text: '☠️ Une malédiction symbolique t’enveloppe…'
-    }, { quoted: m });
-  }
-};
