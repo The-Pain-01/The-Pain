d7c385d4c8143f9d5c208c9b6bb8f67658b30f02
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 19:17:26 +0200

    Update antibot.js

--- commands/antibot.js
+++ commands/antibot.js
@@ -1,14 +1,33 @@
 export default {
-  name: 'antibot',
-  admin: true,
-  async execute(sock, m, args) {
-    global.antibot ??= {};
-    global.antibot[m.chat] = args[0] === 'on';
 
-    await sock.sendMessage(m.chat, {
-      text: args[0] === 'on'
-        ? '🤖 Les intrus mécaniques seront éliminés.'
-        : '🤖 Les bots sont tolérés.'
-    }, { quoted: m });
-  }
+name:"antibot",
+
+admin:true,
+
+
+async execute(sock,m,args){
+
+
+global.antibot ??= {};
+
+
+global.antibot[m.chat] =
+args[0]?.toLowerCase() === "on";
+
+
+await m.reply(
+
+global.antibot[m.chat]
+
+?
+"🤖 Protection antibot activée."
+
+:
+"🤖 Protection antibot désactivée."
+
+);
+
+
+}
+
 };
