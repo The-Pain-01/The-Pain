93844c1b868e4da97f5313486f4958f7f30c0418
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 19:18:55 +0200

    Update antilink.js

--- commands/antilink.js
+++ commands/antilink.js
@@ -1,25 +1,64 @@
 export default {
-  name: "antilink",
-  async execute(sock, m) {
-    if (!m.isGroup) return;
 
-    if (global.antilinkGroups.has(m.chat)) {
-      global.antilinkGroups.delete(m.chat);
-      await sock.sendMessage(m.chat, {
-        text: `
-╔═══〔 🔓 CHAOS AUTORISÉ 🔓 〕═══╗
-🩸 Les liens sont tolérés… pour l’instant.
+name:"antilink",
+
+admin:true,
+
+
+async execute(sock,m){
+
+
+if(!m.isGroup)
+
+return m.reply(
+"☠️ Cette commande est uniquement pour les groupes."
+);
+
+
+
+if(global.antilinkGroups.has(m.chat)){
+
+
+global.antilinkGroups.delete(
+m.chat
+);
+
+
+await m.reply(
+`
+╔═══〔 🔓 𝐀𝐍𝐓𝐈𝐋𝐈𝐍𝐊 〕═══╗
+
+🩸 Les liens sont maintenant autorisés.
+
 ╚════════════════════╝`
-      });
-    } else {
-      global.antilinkGroups.add(m.chat);
-      await sock.sendMessage(m.chat, {
-        text: `
-╔═══〔 🚫 INTERDIT SACRÉ 🚫 〕═══╗
-☠️ Les liens sont désormais bannis.
-🩸 Toute transgression sera punie.
+);
+
+
+
+}
+
+else{
+
+
+global.antilinkGroups.add(
+m.chat
+);
+
+
+
+await m.reply(
+`
+╔═══〔 🚫 𝐀𝐍𝐓𝐈𝐋𝐈𝐍𝐊 〕═══╗
+
+☠️ Les liens sont maintenant bloqués.
+
 ╚════════════════════╝`
-      });
-    }
-  }
+);
+
+
+}
+
+
+}
+
 };
