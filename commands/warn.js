7c3469c3e91e97c0454d02ab5ea0bbc29842844e
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 19:55:11 +0200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,15 +1,34 @@
 export default {
-  name: 'warn',
-  admin: true,
-  async execute(sock, m) {
-    const user = m.mentionedJid[0];
-    if (!user) return;
 
-    global.warns ??= {};
-    global.warns[user] = (global.warns[user] || 0) + 1;
+name:"warn",
 
-    await sock.sendMessage(m.chat, {
-      text: `⚠️ Avertissement ${global.warns[user]} — l’ombre se rapproche.`
-    }, { quoted: m });
-  }
+admin:true,
+
+
+async execute(sock,m){
+
+
+const user =
+m.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0];
+
+
+if(!user)
+return m.reply("⚠️ Mentionne un utilisateur.");
+
+
+
+global.warns ??= {};
+
+global.warns[user] =
+(global.warns[user] || 0)+1;
+
+
+
+m.reply(
+`⚠️ Avertissement ${global.warns[user]} — l’ombre se rapproche.`
+);
+
+
+}
+
 };
