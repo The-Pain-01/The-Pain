a7617f92f6be83839d63142bb8ff5e03f46de758
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 19:12:00 +0200

    Update admins.js

--- commands/admins.js
+++ commands/admins.js
@@ -1,11 +1,54 @@
 export default {
-  name: 'admins',
+
+  name: "admins",
+
   async execute(sock, m) {
-    const meta = await sock.groupMetadata(m.chat);
-    const admins = meta.participants.filter(p => p.admin).map(a => `• @${a.id.split('@')[0]}`).join('\n');
-    await sock.sendMessage(m.chat, {
-      text: `👑 ADMINS\n${admins}`,
-      mentions: meta.participants.map(p => p.id)
-    }, { quoted: m });
+
+
+    if (!m.isGroup)
+      return m.reply(
+        "☠️ Cette commande fonctionne uniquement en groupe."
+      );
+
+
+    const meta =
+      await sock.groupMetadata(
+        m.chat
+      );
+
+
+    const admins =
+      meta.participants
+      .filter(
+        p => p.admin
+      );
+
+
+    const text =
+`👑 𝐀𝐃𝐌𝐈𝐍𝐒 𝐃𝐔 𝐆𝐑𝐎𝐔𝐏𝐄
+
+${admins
+.map(
+p => `• @${p.id.split("@")[0]}`
+)
+.join("\n")}`;
+
+
+    await sock.sendMessage(
+      m.chat,
+      {
+        text,
+        mentions:
+        admins.map(
+          p => p.id
+        )
+      },
+      {
+        quoted:m
+      }
+    );
+
+
   }
+
 };
