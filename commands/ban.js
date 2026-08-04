30827b5214ef67886c45fdd01b4bcd04613db818
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 19:27:55 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,12 +1,38 @@
 export default {
-  name: 'ban',
-  ownerOnly: true,
-  async execute(sock, m) {
-    const user = m.mentionedJid[0];
-    if (!user) return;
-    global.bannedUsers.add(user);
-    await sock.sendMessage(m.chat, {
-      text: '☠️ L’utilisateur est effacé.'
-    }, { quoted: m });
-  }
+
+name:"ban",
+
+ownerOnly:true,
+
+
+async execute(sock,m){
+
+
+const user =
+m.mentionedJid?.[0];
+
+
+if(!user)
+return m.reply(
+"☠️ Mentionne un utilisateur."
+);
+
+
+
+global.bannedUsers ??=
+new Set();
+
+
+
+global.bannedUsers.add(user);
+
+
+
+await m.reply(
+"☠️ Utilisateur ajouté à la liste noire."
+);
+
+
+}
+
 };
