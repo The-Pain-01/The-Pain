9f16d44320f1cf96837588384e36895892c4074d
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 19:01:47 +0200

    Update warnMessages.js

--- system/warnMessages.js
+++ system/warnMessages.js
@@ -1,13 +1,16 @@
 export const WARN_MESSAGES = {
 
   PRIVATE_MODE:
-    "☠️ THE PAIN SYSTEM IS PRIVATE",
+    "☠️ 𝐓𝐇𝐄 𝐏𝐀𝐈𝐍-𝐌𝐃 est en mode privé.",
+
 
   OWNER_ONLY:
-    cmd =>
-      `☠️ ONLY OWNER CAN USE .${cmd}`,
+    (cmd = "") =>
+      `☠️ Cette commande est réservée au propriétaire.\nCommande : ${cmd ? "." + cmd : "inconnue"}`,
+
 
   ADMIN_ONLY:
-    cmd =>
-      `☠️ ONLY ADMINS CAN USE .${cmd}`
+    (cmd = "") =>
+      `☠️ Cette commande est réservée aux administrateurs du groupe.\nCommande : ${cmd ? "." + cmd : "inconnue"}`
+
 };
