98c092d14f702cfe17b5a628e1921a27688d1059
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 19:16:59 +0200

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,40 +1,69 @@
+import config from "../config.js";
+
 export default {
-  name: 'alive',
+
+  name: "alive",
+
   async execute(sock, m) {
-    const BOT_NAME = global.BOT_NAME || '𝐓𝐇𝐄 𝐏𝐀𝐈𝐍-MD';
-    const mode = global.mode || 'public';
+
 
     const text = `
-⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻
-☠️ ${BOT_NAME} ☠️
-⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻
+⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻
+
+☠️ ${config.BOT_NAME} ☠️
+
+⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻
 
 🤖 Status : ONLINE
-⚙️ Mode : ${mode.toUpperCase()}
+
+⚙️ Mode : ${config.MODE.toUpperCase()}
+
 ⏳ Uptime : ${Math.floor(process.uptime())}s
 
+
 🩸 Le système est actif.
+
 🧊 Accès contrôlé selon le mode.
 
-> _The power of 𝐓𝐇𝐄_𝐏𝐀𝐈𝐍-MD flows through this bot_
-⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻
+
+> The power of 𝐓𝐇𝐄_𝐏𝐀𝐈𝐍-MD flows through this bot
+
+⸻⸻⸻⸻⸻⸻⸻⸻
 `;
+
 
     await sock.sendMessage(
       m.chat,
       {
         text,
+
         contextInfo: {
-          isForwarded: true,
-          forwardingScore: 999,
-          forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363422649925479@newsletter',
-            newsletterName: '⏤͟͟͞𝐓𝐇𝐄 亗 𝐏𝐀𝐈𝐍 亗 𝐓𝐄𝐂𝐇᭄',
-            serverMessageId: 1
+
+          isForwarded:true,
+
+          forwardingScore:999,
+
+          forwardedNewsletterMessageInfo:{
+
+            newsletterJid:
+            "120363422649925479@newsletter",
+
+            newsletterName:
+            "⏤͟͟͞𝐓𝐇𝐄 亗 𝐏𝐀𝐈𝐍 亗 𝐓𝐄𝐂𝐇᭄",
+
+            serverMessageId:1
+
           }
+
         }
+
       },
-      { quoted: m }
+      {
+        quoted:m
+      }
     );
+
+
   }
+
 };
