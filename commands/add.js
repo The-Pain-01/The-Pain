abc78eff31bc453f03a2b195268be7ead2b30135
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 19:11:40 +0200

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -1,12 +1,46 @@
 export default {
-  name: 'add',
+
+  name: "add",
+
   admin: true,
+
   async execute(sock, m, args) {
-    const number = args[0]?.replace(/\D/g, '');
-    if (!number) return;
-    await sock.groupParticipantsUpdate(m.chat, [`${number}@s.whatsapp.net`], 'add');
-    await sock.sendMessage(m.chat, {
-      text: '🩸 Une âme est invoquée.'
-    }, { quoted: m });
+
+    if (!m.isGroup)
+      return m.reply("☠️ Cette commande est disponible uniquement en groupe.");
+
+    const number =
+      args[0]?.replace(/\D/g, "");
+
+
+    if (!number)
+      return m.reply("📌 Utilisation : .add 243XXXXXXXXX");
+
+
+    try {
+
+      await sock.groupParticipantsUpdate(
+        m.chat,
+        [
+          `${number}@s.whatsapp.net`
+        ],
+        "add"
+      );
+
+
+      await m.reply(
+        "🩸 Une nouvelle présence rejoint le groupe."
+      );
+
+
+    } catch (err) {
+
+      await m.reply(
+        "❌ Impossible d'ajouter ce contact."
+      );
+
+    }
+
   }
+
 };
