749f937fe5dc05aef885ea28584aa366b2fb6f37
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 19:26:20 +0200

    Update aura.js

--- commands/aura.js
+++ commands/aura.js
@@ -1,25 +1,74 @@
 import fs from "fs";
 
+const DIR = "./data";
 const FILE = "./data/aura.json";
-if (!fs.existsSync(FILE)) fs.writeFileSync(FILE, "{}");
+
+
+if (!fs.existsSync(DIR)) {
+  fs.mkdirSync(DIR, { recursive:true });
+}
+
+
+if (!fs.existsSync(FILE)) {
+  fs.writeFileSync(FILE, "{}");
+}
+
 
 export default {
-  name: "aura",
-  async execute(sock, m) {
-    const data = JSON.parse(fs.readFileSync(FILE));
-    const user = m.sender;
 
-    if (!data[user]) data[user] = 0;
-    data[user] += 1;
+name:"aura",
 
-    fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
 
-    const level = Math.floor(data[user] / 10);
+async execute(sock,m){
 
-    m.reply(
+
+let data = {};
+
+
+try {
+data = JSON.parse(
+fs.readFileSync(FILE)
+);
+}
+catch{
+data = {};
+}
+
+
+
+const user = m.sender;
+
+
+if(!data[user])
+data[user]=0;
+
+
+data[user]++;
+
+
+
+fs.writeFileSync(
+FILE,
+JSON.stringify(data,null,2)
+);
+
+
+
+const level =
+Math.floor(data[user]/10);
+
+
+
+m.reply(
 `╔═══ 🩸 𝐀𝐔𝐑𝐀 ═══╗
+
 ┃ XP : ${data[user]}
 ┃ Niveau : ${level}
-╚═══════════════╝`);
-  }
+
+╚═══════════════╝`
+);
+
+
+}
+
 };
