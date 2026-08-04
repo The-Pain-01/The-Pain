ee4a05eaacd488c088eb2b09d30cbf272794ca04
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 18:59:49 +0200

    Update contextInfo.js

--- system/contextInfo.js
+++ system/contextInfo.js
@@ -7,32 +7,36 @@ export const channelContextInfo = {
   forwardedNewsletterMessageInfo: {
 
     newsletterJid:
-      '120363422649925479@newsletter',
+      "120363422649925479@newsletter",
 
     newsletterName:
-      '⏤͟͟͞𝐓𝐇𝐄 亗 𝐏𝐀𝐈𝐍 亗 𝐓𝐄𝐂𝐇᭄',
+      "⏤͟͟͞𝐓𝐇𝐄 亗 𝐏𝐀𝐈𝐍 亗 𝐓𝐄𝐂𝐇᭄",
 
     serverMessageId: 1
+
   },
+
 
   externalAdReply: {
 
     title:
-      '☠️ 𝐓𝐇𝐄_𝐏𝐀𝐈𝐍-𝐌𝐃 ☠️',
+      "☠️ 𝐓𝐇𝐄_𝐏𝐀𝐈𝐍-𝐌𝐃 ☠️",
 
     body:
-      '🩸 PAIN SYSTEM ACTIVE',
+      "🩸 PAIN SYSTEM ACTIVE",
 
     thumbnailUrl:
-      'https://files.catbox.moe/v72ssh.png',
+      "https://files.catbox.moe/v72ssh.png",
 
     sourceUrl:
-      'https://whatsapp.com/channel/0029Vb7FTvDICVfgeK27ul2S',
+      "https://whatsapp.com/channel/0029Vb7FTvDICVfgeK27ul2S",
 
     mediaType: 1,
 
-    renderLargerThumbnail: false,
+    renderLargerThumbnail: true,
 
     showAdAttribution: true
+
   }
+
 };
