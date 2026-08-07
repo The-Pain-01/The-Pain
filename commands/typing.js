8a759ed5239501bf6696b465f5a885d13cd9dbc3
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-07 21:28:02 +0200

    Delete typing.js

--- commands/typing.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'typing',
-  async execute(sock, m) {
-    await sock.sendPresenceUpdate('composing', m.chat);
-    setTimeout(() => {
-      sock.sendPresenceUpdate('paused', m.chat);
-    }, 5000);
-  }
-};
