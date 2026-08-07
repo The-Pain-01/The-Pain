c0aea3886391448b17ad4b0ee43de4bb31525357
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-07 21:27:33 +0200

    Delete unmute.js

--- commands/unmute.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "unmute",
-  async execute(sock, m) {
-    if (!m.isGroup) return;
-    global.mutedGroups.delete(m.chat);
-    await sock.sendMessage(m.chat, { text: "🔊 Groupe réactivé." });
-  }
-};
