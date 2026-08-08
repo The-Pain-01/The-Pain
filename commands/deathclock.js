7941dd8ace420b8cc7dff447cc0657c19f7c33b8
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:14:47 +0200

    Delete deathclock.js

--- commands/deathclock.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'deathclock',
-  async execute(sock, m) {
-    await sock.sendMessage(m.chat, {
-      text: '⏳ Le temps s’écoule… pour tout le monde.'
-    }, { quoted: m });
-  }
-};
