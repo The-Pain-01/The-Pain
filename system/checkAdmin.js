c245d8796cd016dda1e9c7cb78dcc0f71af8b326
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:53:14 +0200

    Update checkAdmin.js

--- system/checkAdmin.js
+++ system/checkAdmin.js
@@ -1,32 +1,46 @@
-export async function checkAdmin(sock, chat, user) {
-
-  try {
-
-    if (!chat.endsWith("@g.us"))
-      return false;
-
-
-    const meta =
-      await sock.groupMetadata(chat);
+export async function checkAdmin(
+sock,
+chat,
+user
+){
 
 
-    const admins =
-      meta.participants
-      .filter(
-        p => p.admin
-      )
-      .map(
-        p => p.id
-      );
+try{
 
 
-    return admins.includes(user);
+if(!chat.endsWith("@g.us"))
+return false;
 
 
-  } catch (err) {
 
-    return false;
+const metadata =
+await sock.groupMetadata(chat);
 
-  }
+
+
+const admins =
+metadata.participants
+.filter(
+p=>p.admin
+)
+.map(
+p=>p.id
+);
+
+
+
+return admins.includes(user);
+
+
+
+}
+catch{
+
+
+return false;
+
+
+}
+
 
 }
