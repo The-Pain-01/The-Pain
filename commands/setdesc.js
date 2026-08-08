f414f3c5dfaa8f5686c14d9d51588c3377ea9fa4
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:16:22 +0200

    Delete setdesc.js

--- commands/setdesc.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'setdesc',
-  admin: true,
-  async execute(sock, m, args) {
-    await sock.groupUpdateDescription(m.chat, args.join(' '));
-    await sock.sendMessage(m.chat, { text: '🩸 Description scellée.' }, { quoted: m });
-  }
-};
