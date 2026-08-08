5ab63fef60adacf4c46721335b198ca77eb687be
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:16:42 +0200

    Delete setname.js

--- commands/setname.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'setname',
-  admin: true,
-  async execute(sock, m, args) {
-    await sock.groupUpdateSubject(m.chat, args.join(' '));
-    await sock.sendMessage(m.chat, { text: '🩸 Nom du groupe gravé.' }, { quoted: m });
-  }
-};
