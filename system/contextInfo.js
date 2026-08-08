59c9c4acc5135a900d506d516c9b92db607291f7
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:56:05 +0200

    Update contextInfo.js

--- system/contextInfo.js
+++ system/contextInfo.js
@@ -1,42 +1,61 @@
 export const channelContextInfo = {
 
-  forwardingScore: 999,
 
-  isForwarded: true,
-
-  forwardedNewsletterMessageInfo: {
-
-    newsletterJid:
-      "120363422649925479@newsletter",
-
-    newsletterName:
-      "⏤͟͟͞𝐓𝐇𝐄 亗 𝐏𝐀𝐈𝐍 亗 𝐓𝐄𝐂𝐇᭄",
-
-    serverMessageId: 1
-
-  },
+forwardingScore:999,
 
 
-  externalAdReply: {
+isForwarded:true,
 
-    title:
-      "☠️ 𝐓𝐇𝐄_𝐏𝐀𝐈𝐍-𝐌𝐃 ☠️",
 
-    body:
-      "🩸 PAIN SYSTEM ACTIVE",
 
-    thumbnailUrl:
-      "https://files.catbox.moe/v72ssh.png",
+forwardedNewsletterMessageInfo:{
 
-    sourceUrl:
-      "https://whatsapp.com/channel/0029Vb7FTvDICVfgeK27ul2S",
 
-    mediaType: 1,
+newsletterJid:
+"120363422649925479@newsletter",
 
-    renderLargerThumbnail: true,
 
-    showAdAttribution: true
+newsletterName:
+"⏤͟͟͞𝐓𝐇𝐄 亗 𝐏𝐀𝐈𝐍 亗 𝐓𝐄𝐂𝐇᭄",
 
-  }
+
+serverMessageId:1
+
+
+},
+
+
+
+externalAdReply:{
+
+
+title:
+"☠️ 𝐓𝐇𝐄 𝐏𝐀𝐈𝐍 𝐌𝐃 ☠️",
+
+
+body:
+"🩸 PAIN SYSTEM ACTIVE",
+
+
+thumbnailUrl:
+"https://files.catbox.moe/v72ssh.png",
+
+
+sourceUrl:
+"https://whatsapp.com/channel/0029Vb7FTvDICVfgeK27ul2S",
+
+
+mediaType:1,
+
+
+renderLargerThumbnail:true,
+
+
+showAdAttribution:true
+
+
+}
+
+
 
 };
