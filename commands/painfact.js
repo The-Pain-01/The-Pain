57252f57b5a6ef78ac7d455db75e70ba11ac9f5c
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:18:10 +0200

    Delete painfact.js

--- commands/painfact.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'painfact',
-  async execute(sock, m) {
-    await sock.sendMessage(m.chat, {
-      text: '🩸 La douleur forge les plus grandes légendes.'
-    }, { quoted: m });
-  }
-};
