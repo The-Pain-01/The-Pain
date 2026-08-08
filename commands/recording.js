aac4c7195011c74db6735f43c01dd9ba7cd0f2d1
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:17:12 +0200

    Delete recording.js

--- commands/recording.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'recording',
-  async execute(sock, m) {
-    await sock.sendPresenceUpdate('recording', m.chat);
-    setTimeout(() => {
-      sock.sendPresenceUpdate('paused', m.chat);
-    }, 5000);
-  }
-};
