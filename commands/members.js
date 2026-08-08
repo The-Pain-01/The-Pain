8d3a7420759dc9e464f4139e8866dcdca6cda213
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:21:58 +0200

    Delete members.js

--- commands/members.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'members',
-  async execute(sock, m) {
-    const meta = await sock.groupMetadata(m.chat);
-    await sock.sendMessage(m.chat, {
-      text: `👥 Membres : ${meta.participants.length}`
-    }, { quoted: m });
-  }
-};
