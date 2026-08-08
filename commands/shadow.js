472849e081ac78c773f2add142d8aea22d149f9f
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:16:36 +0200

    Delete shadow.js

--- commands/shadow.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'shadow',
-  async execute(sock, m) {
-    await sock.sendMessage(m.chat, {
-      text: '👁️‍🗨️ Une ombre se déplace derrière toi.'
-    }, { quoted: m });
-  }
-};
