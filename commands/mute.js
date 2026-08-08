340dbd33c3ce098d24adb55b6cbf22aae4f3e200
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:21:35 +0200

    Delete mute.js

--- commands/mute.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "mute",
-  async execute(sock, m) {
-    if (!m.isGroup) return;
-    global.mutedGroups.add(m.chat);
-    await sock.sendMessage(m.chat, { text: "🔇 Groupe muté." });
-  }
-};
