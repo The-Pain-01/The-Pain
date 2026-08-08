2355edfa592fe60acc313e439bfd742ce05b0248
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:20:24 +0200

    Delete insult.js

--- commands/insult.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'insult',
-  async execute(sock, m) {
-    await sock.sendMessage(m.chat, {
-      text: '😈 Même l’ombre te juge médiocre.'
-    }, { quoted: m });
-  }
-};
