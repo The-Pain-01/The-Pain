98c2cceb2385515f0bbdbaf74e4a313a1f4971f7
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:55:18 +0200

    Update botAssets.js

--- system/botAssets.js
+++ system/botAssets.js
@@ -1,40 +1,55 @@
 import config from "../config.js";
 
+
 export const BOT_NAME =
-  config.BOT_NAME || "𝐓𝐇𝐄_𝐏𝐀𝐈𝐍-MD";
+config.BOT_NAME;
+
 
 export const DEV =
-  config.OWNER_NAME || "𝐓𝐇𝐄 𝐏𝐀𝐈𝐍";
+config.OWNER_NAME;
+
 
 export const CHANNEL =
-  config.CHANNEL || "";
+config.CHANNEL;
+
+
 
 export const IMAGES = {
 
-  menu:
-    config.MENU_IMAGE || "",
 
-  support:
-    config.SUPPORT_IMAGE || "",
+menu:
+config.MENU_IMAGE,
 
-  botinfo:
-    config.BOTINFO_IMAGE || "",
 
-  owner:
-    config.OWNER_IMAGE || "",
+support:
+config.SUPPORT_IMAGE,
 
-  connect:
-    config.CONNECT_IMAGE || ""
+
+botinfo:
+config.BOTINFO_IMAGE,
+
+
+owner:
+config.OWNER_IMAGE,
+
+
+connect:
+config.CONNECT_IMAGE
+
 
 };
 
 
+
 export const AUDIOS = {
 
-  menu:
-    config.MENU_AUDIO || "",
 
-  connect:
-    config.CONNECT_AUDIO || ""
+menu:
+config.MENU_AUDIO,
+
+
+connect:
+config.CONNECT_AUDIO
+
 
 };
