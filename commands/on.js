108339b8ae29e4f8a2f0ffad8bc429dd8d067730
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-08 03:18:33 +0200

    Delete on.js

--- commands/on.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'on',
-  admin: true,
-  async execute(sock, m) {
-    global.disabledGroups.delete(m.chat);
-    await sock.sendMessage(m.chat, { text: '🩸 𝐓𝐇𝐄_𝐏𝐀𝐈𝐍-MD est réveillé.' }, { quoted: m });
-  }
-};
